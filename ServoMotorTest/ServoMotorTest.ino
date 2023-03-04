d0ecccde4ee9bb63655e746e61ae37664365420d
rakinrkz <rakinuzzaman@gmail.com>
2023-03-04 12:20:00 +0600

    modified for ppm value instead of angle value

--- ServoMotorTest/ServoMotorTest.ino
+++ ServoMotorTest/ServoMotorTest.ino
@@ -1,26 +1,30 @@
 #include <Servo.h>  // include the servo library
+#define servoPin 3 //this is the pin to connect to servo signal wire
 
 Servo myservo;  // create a servo object
 
 void setup() {
-  myservo.attach(9);  // attach the servo to pin 9
+  myservo.attach(servoPin);     // attach the servo to pin 9
   Serial.begin(115200);  // start the serial communication
 
-  Serial.println("Enter value between 0 and 180 to control the servo motor...");
-  Serial.println("And Select No Line Ending")
+  Serial.println("Enter value between 1000 and 2000 to control the servo motor...");
+  Serial.println("And Select No Line Ending");
 }
 
 void loop() {
-  int angle;  // variable to store the angle
+  int value;  // variable to store the angle
 
-  if (Serial.available() > 0) 
-  {  // check if there is data available from the serial port
-     Serial.println("got data from serial");
-    angle = Serial.parseInt();  // read the angle from the serial port
+  if (Serial.available() > 0) {  // check if there is data available from the serial port
+    Serial.println("got data from serial");
+    int x = Serial.parseInt();  // read the angle from the serial port
+    Serial.print("data read: ");
+    Serial.println(x);
 
-    myservo.write(angle);  // set the servo position
+    if (x >= 100 and x <= 2900)
+      value = x;
   }
-
-  Serial.println(angle);
-  delay(1000);
+  myservo.writeMicroseconds(value);  // set the servo position
+  
+  Serial.println(value);
+  delay(100);
 }
